a6b143f6cf81916614ed30655c5428dea9872822
Wei Dong <wdong@wdong.org>
2017-01-01 23:14:15 -0500

    update

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.wdong.org/'
+SITEURL = 'http://www.wdong.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
